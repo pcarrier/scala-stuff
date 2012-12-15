a7c9a50a73c1954bed2d0b4f8639018e3a1dccd1
Pierre Carrier <pierre@spotify.com>
2012-12-15 23:21:07 +0100

    bottles: let's look at the generated bytecode

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "scala-stuff"
+
+version := "1.0"
+
+scalaVersion := "2.10.0-RC1"
---
+++ src/main/scala/bottles.scala
@@ -0,0 +1,5 @@
+object Bottles {
+  def main(args: Array[String]) =
+    for (n <- (1 to 100))
+      println(s"$n bottles!")
+}
